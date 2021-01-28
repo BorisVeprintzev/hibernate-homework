4f97a63bb76006cc31eaf22134e7e69433b0271a
Boris <boris.veprintsev@yandex.ru>
2021-01-28 15:03:58 +0300

    full

--- src/main/java/ru/hh/school/entity/Resume.java
+++ src/main/java/ru/hh/school/entity/Resume.java
@@ -1,29 +1,34 @@
 package ru.hh.school.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-//TODO: оформите entity
+
+@Entity
+@Table(name = "resume")
 public class Resume {
-  // TODO: сделать так, чтобы id брался из sequence-а
-  // таким образом, мы сможем отправлять в бд запросы батчами.
-  // нужно учитывать, что описание sequence при создании таблицы также должно соответствовать
-  // хиберовской сущности (см. create_resume.sql)
-  //
-  // Подробнее:
-  // https://vladmihalcea.com/how-to-batch-insert-and-update-statements-with-hibernate/
-  // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
 
-  @Id
-  @GeneratedValue(/* здесь место для вашего кода */)
-  private Integer id;
+    // таким образом, мы сможем отправлять в бд запросы батчами.
+    // нужно учитывать, что описание sequence при создании таблицы также должно соответствовать
+    // хиберовской сущности (см. create_resume.sql)
+    //
+    // Подробнее:
+    // https://vladmihalcea.com/how-to-batch-insert-and-update-statements-with-hibernate/
+    // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
 
-  private String description;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="resume_id_seq")
+    @SequenceGenerator(name = "resume_gen", sequenceName = "resume_id_seq", allocationSize = 10)
+    @Column(name = "id")
+    private Integer id;
 
-  Resume() {}
 
-  public Resume(String description) {
-    this.description = description;
-  }
+    private String description;
+
+    Resume() {
+    }
+
+    public Resume(String description) {
+        this.description = description;
+    }
 
 }
